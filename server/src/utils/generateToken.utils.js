66e4e78c2e2dcf201216fcb2d289e50f2ceb9da0
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-12-26 22:25:53 +0530

    feat: Implement token generation utility for JWT authentication
    - Added `generateToken.utils.js` to encapsulate the logic for generating JSON Web Tokens (JWT) and setting them as cookies in the response.
    - The `generateToken` function:
      - Accepts the response object, user data, and a message as parameters.
      - Constructs a token payload containing the user's ID and email.
      - Utilizes the `jsonwebtoken` library to create a signed token using a secret key and expiration time defined in environment variables.
      - Configures cookie options to enhance security, including `httpOnly`, `sameSite`, and `secure` attributes, with a maximum age of 1 day for the cookie.
      - Sends a JSON response containing the success status, message, user data, and the generated token.
    
    This utility streamlines the authentication process by providing a reusable function for token generation and cookie management, improving code organization and security practices within the application.

---
+++ server/src/utils/generateToken.utils.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+
+// NOTE - Generate Token
+export function generateToken(response, user, message) {
+    const tokenPayload = {
+        userId: user._id,
+        email: user.email,
+    }
+
+    // NOTE - Token Creation
+    const token = jwt.sign(tokenPayload, String(process.env.JWT_SECRET_KEY), {
+        expiresIn: String(process.env.JWT_EXPIRE)
+    })
+
+    // NOTE - Cookie Creation
+    const cookieOptions = {
+        httpOnly: true,
+        sameSite: 'strict',
+        maxAge: 24 * 60 * 60 * 1000,    // Output: 1 day
+        secure: true,
+    }
+
+    return response
+        .staus(200)
+        .cookie('token', token, cookieOptions)
+        .json({
+            success: true,
+            message,
+            user,
+            token
+        })
+}
