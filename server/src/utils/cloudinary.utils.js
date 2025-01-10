10e08582b6436d9f8ed21d4def35180c9ffe231f
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-01-10 21:16:54 +0530

    fix: Correct environment variable name for Cloudinary configuration
    This commit addresses a typo in the Cloudinary configuration within the `cloudinary.utils.js` file. The environment variable for the Cloudinary cloud name was incorrectly referenced as `CLOUDINARY_ClOUD_NAME`, which contained an erroneous uppercase 'O'.
    
    The variable has been corrected to `CLOUDINARY_CLOUD_NAME`, ensuring that the application can properly access the correct Cloudinary account settings. This fix is crucial for the successful integration of Cloudinary services, as it prevents potential runtime errors related to undefined environment variables.
    
    Changes made:
    - Updated the environment variable name from `CLOUDINARY_ClOUD_NAME` to `CLOUDINARY_CLOUD_NAME` in the Cloudinary configuration section.
    
    This change enhances the reliability of the application by ensuring that the correct environment variable is used for Cloudinary integration.

--- server/src/utils/cloudinary.utils.js
+++ server/src/utils/cloudinary.utils.js
@@ -5,7 +5,7 @@ dotenv.config({})    // NOTE - Check and Load environment variables from .env fi
 
 // NOTE - Configure Cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_ClOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
