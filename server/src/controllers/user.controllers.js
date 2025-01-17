36f24bdf3ac19af4457fc7668bf20e7658815b1f
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-01-17 14:27:09 +0530

    feat: Implement user password management and account deletion functionality
    - Added `changeUserPassword` function to allow users to update their passwords securely. This includes:
      - Validating user ID and ensuring the user exists.
      - Checking for the presence of current and new passwords.
      - Verifying the current password against the stored hash.
      - Ensuring the new password is different from the current password and matches the confirmation.
      - Hashing the new password before saving it to the database.
      - Updating the user's last active date and clearing existing sessions.
      - Generating a success token upon successful password change.
    
    - Introduced `forgotPassword` function to handle password reset requests:
      - Validates the provided email and checks for the existence of the user.
      - Generates a password reset token and constructs a reset URL.
      - Sends a password reset email to the user with the reset link.
      - Updates the user's last active date and handles potential errors during email sending.
    
    - Implemented `deleteUserAccount` function to allow users to delete their accounts:
      - Validates the user ID and checks for its existence.
      - Cleans up related data in the `Course` and `UserRating` collections to maintain data integrity.
      - Deletes the user account from the database and sends a success response.
    
    - Enhanced error handling throughout the new functions to provide meaningful feedback to users in case of failures.
    
    This commit enhances user account management capabilities, improving security and user experience.

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -2,6 +2,8 @@ import { catchAsync, ApiError } from "../middleware/error.middleware.js";
 import { User } from '../models/user.models.js'
 import { generateToken } from "../utils/generateToken.utils.js";
 import { deleteMediaFromCloudinary, uploadMedia } from "../utils/cloudinary.utils.js";
+import { Course } from "../models/course.models.js";
+import { UserRating } from "../models/userRating.models.js";
 
 
 
@@ -225,7 +227,192 @@ export const updateUserProfile = catchAsync(async function (request, response) {
 
 
 
-// NOTE - Update current user profile
-// export const test = catchAsync(async function (request, response) {
+// NOTE - Change user password
+export const changeUserPassword = catchAsync(async function (request, response) {
+    // NOTE - Get user id from request id
+    const { userId } = request.id
 
-// })
+    // NOTE - Check for invalid user id
+    if (!userId) {
+        throw new ApiError('Invalid user id', 404)
+    }
+
+    // NOTE - Check and get user using userId from database
+    const user = await User.findById(userId)
+
+    // NOTE - Check for invalid user
+    if (!user) {
+        throw new ApiError('User not found', 404)
+    }
+
+    // NOTE - Get Passwords from request body
+    const { currentPassword, newPassword, confirmPassword } = request.body
+
+    // NOTE - Check for invalid password
+    if (!currentPassword || !newPassword || !confirmPassword) {
+        throw new ApiError('Please provide all the required fields', 400)
+    }
+
+    // NOTE - Check for old password mismatch
+    const isOriginalPasswordCorrect = await user.comparePassword(currentPassword)
+
+    if (!isOriginalPasswordCorrect) {
+        throw new ApiError('Old password is incorrect', 400)
+    }
+
+    // NOTE - Check for Old password and new password duplication
+    if (currentPassword === newPassword) {
+        throw new ApiError('New password cannot be the same as old password', 400)
+    }
+
+
+    // NOTE - Check for new password and confirm password mismatch
+    if (newPassword !== confirmPassword) {
+        throw new ApiError('New password and confirm password does not match', 400)
+    }
+
+    // NOTE - Hash new password before database save
+    const salt = await bcrypt.genSalt(10)
+    const hashedNewPassword = await bcrypt.hash(newPassword, salt)
+
+
+    // NOTE - Update user password
+    user.password = hashedNewPassword
+    await user.save({ validateBeforeSave: false })
+
+
+    // NOTE - Wire update last accessed date to user
+    await user.updateLastActive()
+
+    // NOTE - Clear any existing sessions [cookies]
+    await user.clearSessions()
+
+    // NOTE - Generate token and send response
+    generateToken(response, user, 'Password changed successfully')
+})
+
+
+
+
+// NOTE - Request for password reset
+export const forgotPassword = catchAsync(async function (request, response) {
+    // NOTE - Get email from request body
+    const { email } = request.body
+
+    // NOTE - Check for invalid email
+    if (!email) {
+        throw new ApiError('Please provide email', 400)
+    }
+
+    // NOTE - Check for existing user in database
+    const user = await User.findOne({ email: email.toLowerCase() })
+
+    // NOTE - Throw error if user not found
+    if (!user) {
+        throw new ApiError('User not found', 404)
+    }
+
+    // NOTE - Generate reset password token
+    const resetPasswordToken = user.schema.methods.generatePasswordResetToken.call(user)
+
+
+    /* NOTE - .call(user) method:  
+
+    The .call(user) method is used to explicitly set the this context of the defined function.
+
+    In JavaScript, when a function is called, the |this| context is set to the object that the function is a property of. For example, if you have an object |obj| with a method |method|, and you call |obj.method()|, the |this| context inside method will be set to obj.
+
+    However, when you retrieve a document from the database using Mongoose, the resulting object is not an instance of the Mongoose model, but rather a plain JavaScript object. This object does not have any of the methods or properties defined on the Mongoose model.
+
+    In the case of the getResetPasswordToken method, it is defined on the Mongoose model's schema using the methods property. When dev retrieve a document from the database, the resulting object does not have the getResetPasswordToken method attached to it.
+
+    To call the getResetPasswordToken method on the retrieved document, dev needs to explicitly set the |this| context of the method to the retrieved document. This is what the .call(user) method does.
+
+    */
+
+
+    // NOTE - send password reset email
+    const resetPasswordURL = `${process.env.CLIENT_URL}/reset-password/${resetPasswordToken}`
+
+    const message = `Please click on the following link to reset your password: ${resetPasswordURL}`
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password Reset Request',
+            message
+        })
+
+
+        // NOTE - Delete user password reset token
+        user.removeResetPasswordTokenAndExpire()
+
+        // NOTE - Wire update last accessed date to user
+        await user.updateLastActive()
+
+        // NOTE - Send response
+        return response
+            .status(200)
+            .json({
+                success: true,
+                message: 'Password reset email sent successfully',
+                resetPasswordURL,
+                resetPasswordToken,
+            })
+
+    } catch (error) {
+
+        // NOTE - Delete user password reset token
+        user.removeResetPasswordTokenAndExpire()
+
+        // NOTE - Save changes to database
+        await user.save({ validateBeforeSave: false })
+
+        throw new ApiError('Email could not be sent', 500)
+    }
+})
+
+
+
+
+// NOTE - Delete user account
+export const deleteUserAccount = catchAsync(async function (request, response) {
+
+    // NOTE - Get user id from request
+    const { userId } = request.id
+
+    // NOTE - Check for invalid user id
+    if (!userId) {
+        throw new ApiError('User not found', 404)
+    }
+
+    try {
+        // NOTE - Remove user info from related collections
+        await Promise.all([
+            // NOTE - Cleanup user profile
+            await Course.updateMany(
+                { enrolledStudents: userId },
+                { $pull: { enrolledStudents: userId } }
+            ),
+
+            await UserRating.deleteMany({ user: userId }),
+        ])
+
+
+        // NOTE - Delete user
+        await User.findByIdAndDelete(userId)
+
+
+        // NOTE - Send response
+        return response
+            .status(200)
+            .json({
+                success: true,
+                message: 'User account deleted successfully'
+            })
+    } catch (error) {
+        console.error("Error deleting user account\n", error.message);
+
+        throw new ApiError('User account could not be deleted, Please try again later.', 500)
+    }
+})
